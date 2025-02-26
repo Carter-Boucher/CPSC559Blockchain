8c51ddff00453a7cfb8ef52dd05f1ffd868a9d7f
Carter Boucher <carterboucher85@gmail.com>
2025-02-26 13:22:50 -0700

    client code

--- startnodes.bat
+++ startnodes.bat
@@ -1,3 +1,3 @@
 start python blockchain.py --host 127.0.0.1 --port 5000 --peers "http://127.0.0.1:5001,http://127.0.0.1:5002"
 start python blockchain.py --host 127.0.0.1 --port 5001 --peers "http://127.0.0.1:5000,http://127.0.0.1:5002"
-start python blockchain.py --host 127.0.0.1 --port 5002 --peers "http://127.0.0.1:5000,http://127.0.0.1:5001"
+::start python blockchain.py --host 127.0.0.1 --port 5002 --peers "http://127.0.0.1:5000,http://127.0.0.1:5001"
