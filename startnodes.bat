9d25b1b5f12130690cbcdba80765ba94a33d97ea
Carter Boucher <carterboucher85@gmail.com>
2025-03-11 23:28:24 -0600

    Batch file

--- startnodes.bat
+++ startnodes.bat
@@ -1,5 +1,2 @@
-@REM start python blockchain.py --host 127.0.0.1 --port 5000
-@REM start python blockchain.py --host 127.0.0.1 --port 5001 --peers 127.0.0.1:5000
-
 start python main.py --host 127.0.0.1 --port 5000
 start python main.py --host 127.0.0.1 --port 5001 --peers 127.0.0.1:5000
