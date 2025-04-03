4baf872c7345e4cb3fa7962ed30678a436e71270
Carter Boucher <carterboucher85@gmail.com>
2025-04-02 23:09:46 -0600

    Updated consensus mechanism and most comulative work chain

--- laptopNodes.bat
+++ laptopNodes.bat
@@ -1,2 +1,3 @@
 start python main.py --host 100.75.248.88 --port 5000
 start python main.py --host 100.75.248.88 --port 5001 --peers "100.75.248.88:5000"
+start python main.py --host 100.75.248.88 --port 5002 --peers "100.75.248.88:5000"
