01ff02a92d1b29eb2b023be804e857066a9cf587
Carter Boucher <carterboucher85@gmail.com>
2025-04-13 16:38:13 -0600

    morgan start shell

--- morganStart.sh
+++ morganStart.sh
@@ -1,5 +1,4 @@
-echo "1"
-python3 main.py --host 100.73.220.26 --port 5000
 
-echo "2"
-python3 main.py --host 100.73.220.26 --port 5001 --peers "100.73.220.26:5000"
+python3 main.py --host 100.73.220.26 --port 5050 &
+
+python3 main.py --host 100.73.220.26 --port 5001 --peers "100.73.220.26:5050"
